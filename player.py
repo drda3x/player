13a04817248618aed456c00691c53d8745614d10
Нестеров Василий <da3x11@gmail.com>
2015-03-10 20:16:51 +0300

    Теперь он выглядит так, как должен был

--- player.py
+++ player.py
@@ -3,109 +3,101 @@
 
 import datetime
 import os, sys
-from Tkinter import *
-from tkSnack import *
-import tkFileDialog
+from Tkinter import Tk, Button, Frame, Label, Listbox, SINGLE, END, X
+from tkSnack import Sound, initializeSnack
 
-stop_after = 90  #sec
 
-root = Tkinter.Tk()
-
+root = Tk()
 initializeSnack(root)
 
-songs_dir = 'C:\\music\\'
 
-if songs_dir not in sys.path:
-	sys.path.append(songs_dir)
+class Song(object):
+    u"""
+    Класс для работы с песенками
+    """
 
-songs = os.listdir(songs_dir)
-songs_list = Listbox(root, height=300, width=200, selectmode=SINGLE)
-songs_files = {}
-song = {}
+    __stop_time = 90
+    __id = None
 
-options = {}
-options['defaultextension'] = '.mp3'
-options['filetypes'] = [('mp3 files', '.mp3')]
-options['initialdir'] = 'C:\\music\\'
-options['parent'] = root
-options['title'] = 'This is a title'
+    sound = Sound()
 
-k = 0
-def _load():
-	for i in songs:
-		# try:
-		name = u'' + i
-		songs_list.insert(END, name)
+    def play(self):
 
-		options['initialfile'] = name
-		song_file = tkFileDialog.askopenfilename(**options)
+        root.after_cancel(self.__id) if self.__id else None
 
-		song = Sound()
-		songs_files[k] = song.read(song_file)
-		song.play()
-		k += 1
+        self.sound.play()
+        __id = root.after(self.__stop_time * 1000, self.stop)
 
-		yield
-		# except:
-		# 	None
+    def stop(self):
+        root.after_cancel(self.__id) if self.__id else None
+        self.__id = None
+        self.sound.stop()
 
-		# finally:
-		# 	yield
+    def pause(self):
+        self.sound.pause()
 
-# it = _load()
+    def load(self, filename):
+        self.sound.load(filename)
 
-def load():
-	try:
-		it.next()
-		root.after(100, load)
-	except StopIteration:
-		pass
+    def set_stop_time(self, stop_time_value):
+        self.__stop_time = stop_time_value
 
-	# _file = root.tk.call('eval', 'snack::getOpenFile')
-	# song['curent'].read(_file)
+
+class PlayList():
+
+    def __init__(self, frame, path):
+        self.__lb = Listbox(frame, selectmode=SINGLE, height=25, width=90)
+        self.dir = path
+
+        self.songs_list = os.listdir(self.dir)
+        for song in self.songs_list:
+            self.__lb.insert(END, song)
+
+        self.__lb.pack()
+
+    @property
+    def selected(self):
+        try:
+            return self.songs_list[self.__lb.curselection()[0]]
+        except IndexError:
+            return None
+
+
+Frame(root).pack(pady=5)
+upLabel = Label(root, text='My super player', font='Helvetica 12 bold')
+upLabel.pack()
+
+f0 = Frame(root)
+f1 = Frame(root)
+f0.pack(pady=5)
+f1.pack(pady=5)
+
+song = Song()
+play_list = PlayList(f1, u'D:\\Da3x\\Music\\хастл')
+
 
 def play():
 
-	song['curent'] = Sound()
-	song_file = tkFileDialog.askopenfilename(**options)
-	if song_file:
-		song['curent'].read(song_file)
-		song['begin_time'] = datetime.datetime.now()
-		song['curent'].play()
+    stop()
 
-		upLabel.configure(text=song_file.split('/')[-1])
+    filename = play_list.dir + '\\' + play_list.selected if play_list.selected else None
 
-		song['timer'] = True
-		root.after(1000, timer)
-		root.after(stop_after * 1000, stop)
+    if filename:
+        song.load(filename)
+        song.play()
+
 
 def stop():
-	song['timer'] = False
-	song['curent'].stop()
-	play()
+    song.stop()
+
 
 def pause():
-    song['curent'].pause()
+    song.pause()
 
-def timer():
-    dur = str(int((datetime.datetime.now() - song['begin_time']).total_seconds())) + ' sec'
-    timelab.configure(text=dur)
-
-    if song['timer']:
-    	root.after(1000,timer)
-
-Frame(root).pack(pady=5)
-upLabel = Label(root, text='My super player',
-      font='Helvetica 12 bold')
-upLabel.pack()
-f0 = Frame(root)
-f0.pack(pady=5)
-timelab = Label(f0, text='0.00 sec',width=10)
+timelab = Label(f0, text='0.00 sec', width=10)
 timelab.pack(side='left')
 Button(f0, bitmap='snackPlay', command=play, height=50, width=50).pack(side='left')
 Button(f0, bitmap='snackStop', command=stop, height=50, width=50).pack(side='left')
 Button(f0, bitmap='snackPause', command=pause, height=50, width=50).pack(side='left')
 
-# root.after(100, load)
-# songs_list.pack()
 root.mainloop()
