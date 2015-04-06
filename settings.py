9aecb9f2d9a3d162e80b0b609cfeb1e4cd0fc4d9
Нестеров Василий <da3x11@gmail.com>
2015-04-06 15:37:20 +0300

    a

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ def get_music_dir():
     settings_file = file('settings.txt', 'r')
     config = settings_file.read()
     match = re.search('MUSIC_DIR = u\'.*\'$', config)
+    settings_file.close()
 
     if match:
         return match.group()
@@ -19,4 +20,3 @@ def get_music_dir():
 
 
 def set_music_dir(folder):
-    pass
