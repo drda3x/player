e5abe9134e5981dbaa4ecccf4e102a48b9eaff45
Vasily Nesterov <da3x11@gmail.com>
2015-04-06 20:49:03 +0300

    aa

--- settings.py
+++ settings.py
@@ -9,11 +9,11 @@ MUSIC_DIR = u'D:/Da3x/Music/hastle'
 def get_music_dir():
     settings_file = file('settings.txt', 'r')
     config = settings_file.read()
-    match = re.search('MUSIC_DIR = u\'.*\'$', config)
+    match = re.search('MUSIC_DIR = .*$', config)
     settings_file.close()
 
     if match:
-        return match.group().split(' = ')[-1]
+        return u'' + match.group().split(' = ')[-1]
 
     else:
         return None
@@ -25,13 +25,13 @@ def set_music_dir(folder):
     config = settings_file.read()
     settings_file.close()
 
-    match = re.search('MUSIC_DIR = u\'.*\'$', config)
+    match = re.search('MUSIC_DIR = .*$', config)
 
     if match:
-        re.sub('MUSIC_DIR = u\'.*\'$', 'MUSIC_DIR = u\'%s\'\n' % folder, config)
-
+        config = re.sub('MUSIC_DIR = .*$', u'MUSIC_DIR = %s\n' % folder, config)
+        print config
     else:
-        config += 'MUSIC_DIR = u\'%s\'\n' % folder
+        config += u'MUSIC_DIR = %s\n' % folder
 
     settings_file = file('settings.txt', 'w')
     settings_file.write(config)
